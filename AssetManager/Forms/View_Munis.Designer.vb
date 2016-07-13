0b410126bba3749e2dcfd167ea866b129192f153
Bobbar <bobby.lovell@worthingtonindustries.com>
2016-07-13 09:04:15 -0400

    Search MUNIS by serial

--- AssetManager/Forms/View_Munis.Designer.vb
+++ AssetManager/Forms/View_Munis.Designer.vb
@@ -25,6 +25,9 @@ Partial Class View_Munis
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(View_Munis))
         Me.DataGridMunis = New System.Windows.Forms.DataGridView()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.txtSerial = New System.Windows.Forms.TextBox()
+        Me.cmdSearch = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.DataGridMunis, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
@@ -49,7 +52,7 @@ Partial Class View_Munis
         Me.DataGridMunis.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect
         Me.DataGridMunis.ShowCellToolTips = False
         Me.DataGridMunis.ShowEditingIcon = False
-        Me.DataGridMunis.Size = New System.Drawing.Size(1016, 289)
+        Me.DataGridMunis.Size = New System.Drawing.Size(1016, 482)
         Me.DataGridMunis.TabIndex = 41
         '
         'Panel1
@@ -59,16 +62,44 @@ Partial Class View_Munis
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Panel1.Controls.Add(Me.DataGridMunis)
         Me.Panel1.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Location = New System.Drawing.Point(12, 114)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(1022, 295)
+        Me.Panel1.Size = New System.Drawing.Size(1022, 488)
         Me.Panel1.TabIndex = 42
+        '
+        'txtSerial
+        '
+        Me.txtSerial.Location = New System.Drawing.Point(240, 46)
+        Me.txtSerial.Name = "txtSerial"
+        Me.txtSerial.Size = New System.Drawing.Size(125, 20)
+        Me.txtSerial.TabIndex = 43
+        '
+        'cmdSearch
+        '
+        Me.cmdSearch.Location = New System.Drawing.Point(507, 46)
+        Me.cmdSearch.Name = "cmdSearch"
+        Me.cmdSearch.Size = New System.Drawing.Size(129, 24)
+        Me.cmdSearch.TabIndex = 44
+        Me.cmdSearch.Text = "Search"
+        Me.cmdSearch.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(237, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 13)
+        Me.Label1.TabIndex = 45
+        Me.Label1.Text = "Serial"
         '
         'View_Munis
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1046, 319)
+        Me.ClientSize = New System.Drawing.Size(1046, 614)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmdSearch)
+        Me.Controls.Add(Me.txtSerial)
         Me.Controls.Add(Me.Panel1)
         Me.DoubleBuffered = True
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -77,9 +108,13 @@ Partial Class View_Munis
         CType(Me.DataGridMunis, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents DataGridMunis As DataGridView
     Friend WithEvents Panel1 As Panel
+    Friend WithEvents txtSerial As TextBox
+    Friend WithEvents cmdSearch As Button
+    Friend WithEvents Label1 As Label
 End Class
--- AssetManager/Forms/View_Munis.vb
+++ AssetManager/Forms/View_Munis.vb
@@ -11,4 +11,17 @@
     Private Sub View_Munis_Load(sender As Object, e As EventArgs) Handles Me.Load
         ExtendedMethods.DoubleBuffered(DataGridMunis, True)
     End Sub
+
+    Private Sub cmdSearch_Click(sender As Object, e As EventArgs) Handles cmdSearch.Click
+        Dim MunisTable As DataTable
+
+        MunisTable = ReturnMSSQLTable("SELECT TOP 10 * FROM famaster WHERE fama_serial='" & Trim(txtSerial.Text) & "'")
+
+        Dim r As DataRow
+        For Each r In MunisTable.Rows
+            Debug.Print(r.Item("fama_asset"))
+        Next
+        LoadMunisGrid(MunisTable)
+
+    End Sub
 End Class
