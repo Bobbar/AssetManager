92c51c5b93bced105d1cd7cdcca032696cfc1225
Bobbar <bobby.lovell@worthingtonindustries.com>
2016-10-07 13:19:31 -0400

    Fix GetIcon AccessViolation bug in release compile.

--- AssetManager/Helpers/OtherFunctions.vb
+++ AssetManager/Helpers/OtherFunctions.vb
@@ -29,26 +29,28 @@ Module OtherFunctions
         SHGFI_LARGEICON = 0
         SHGFI_SMALLICON = 1
     End Enum
-    <DllImport("shell32.dll", CharSet:=CharSet.Auto)>
-    Private Function SHGetFileInfo(
+    Private Declare Ansi Function SHGetFileInfo Lib "shell32.dll" (
                 ByVal pszPath As String,
                 ByVal dwFileAttributes As Int32,
                 ByRef psfi As SHFILEINFO,
                 ByVal cbFileInfo As Int32,
                 ByVal uFlags As Int32) As IntPtr
-    End Function
     <DllImport("user32.dll", SetLastError:=True)>
     Private Function DestroyIcon(ByVal hIcon As IntPtr) As Boolean
     End Function
     Public Function GetFileIcon(ByVal fileExt As String) As Bitmap ', Optional ByVal ICOsize As IconSize = IconSize.SHGFI_SMALLICON
-        Dim ICOSize As IconSize = IconSize.SHGFI_SMALLICON
-        Dim shinfo As New SHFILEINFO
-        shinfo.szDisplayName = New String(Chr(0), MAX_PATH)
-        shinfo.szTypeName = New String(Chr(0), 80)
-        SHGetFileInfo(fileExt, FILE_ATTRIBUTE_NORMAL, shinfo, Marshal.SizeOf(shinfo), SHGFI_ICON Or ICOSize Or SHGFI_USEFILEATTRIBUTES)
-        Dim bmp As Bitmap = System.Drawing.Icon.FromHandle(shinfo.hIcon).ToBitmap
-        DestroyIcon(shinfo.hIcon) ' must destroy icon to avoid GDI leak!
-        Return bmp ' return icon as a bitmap
+        Try
+            Dim ICOSize As IconSize = IconSize.SHGFI_SMALLICON
+            Dim shinfo As New SHFILEINFO
+            shinfo.szDisplayName = New String(Chr(0), MAX_PATH)
+            shinfo.szTypeName = New String(Chr(0), 80)
+            SHGetFileInfo(fileExt, FILE_ATTRIBUTE_NORMAL, shinfo, Marshal.SizeOf(shinfo), SHGFI_ICON Or ICOSize Or SHGFI_USEFILEATTRIBUTES)
+            Dim bmp As Bitmap = System.Drawing.Icon.FromHandle(shinfo.hIcon).ToBitmap
+            DestroyIcon(shinfo.hIcon) ' must destroy icon to avoid GDI leak!
+            Return bmp ' return icon as a bitmap
+        Catch ex As Exception
+            ErrHandle(ex, System.Reflection.MethodInfo.GetCurrentMethod().Name)
+        End Try
     End Function
     Public Function AdjustComboBoxWidth(ByVal sender As Object, ByVal e As EventArgs)
         Dim senderComboBox = DirectCast(sender, ComboBox)
