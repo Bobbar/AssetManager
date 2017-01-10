Bobbar/AssetManager

commit d0275cb76661b5f1cb9bd4d3a05ffe4edc125343

-Fix weird mouse event hang causing unintented highlighing of rows in ResultGrid.

AssetManager/UserInterface/Forms/Main/MainForm.vb
@@ -291,11 +291,12 @@ Public Class MainForm
             Exit Sub
         End If
         If Not DeviceIsOpen(strGUID) Then
+            ResultGrid.Enabled = False
             Waiting()
             Dim NewView As New frmView(Me, strGUID)
             DoneWaiting()
+            ResultGrid.Enabled = True
         Else
-            ' Dim blah = Message("That device is already open.", vbOKOnly + vbExclamation, "Duplicate Window")
             ActivateFormByUID(strGUID)
         End If
     End Sub
@@ -481,7 +482,6 @@ Public Class MainForm
         End If
     End Sub
     Private Sub ResultGrid_CellMouseDown(sender As Object, e As DataGridViewCellMouseEventArgs) Handles ResultGrid.CellMouseDown
-        On Error Resume Next
         If e.Button = MouseButtons.Right And Not ResultGrid.Item(e.ColumnIndex, e.RowIndex).Selected Then
             ResultGrid.Rows(e.RowIndex).Selected = True
             ResultGrid.CurrentCell = ResultGrid(e.ColumnIndex, e.RowIndex)

commit 9cd3c1ded0ac71728bcb1d5348dc8f2a25f6e25e

-Fix column sorting error caused by d0275cb7.

AssetManager/UserInterface/Forms/Main/MainForm.vb
@@ -482,6 +482,7 @@ Public Class MainForm
         End If
     End Sub
     Private Sub ResultGrid_CellMouseDown(sender As Object, e As DataGridViewCellMouseEventArgs) Handles ResultGrid.CellMouseDown
+        On Error Resume Next
         If e.Button = MouseButtons.Right And Not ResultGrid.Item(e.ColumnIndex, e.RowIndex).Selected Then
             ResultGrid.Rows(e.RowIndex).Selected = True
             ResultGrid.CurrentCell = ResultGrid(e.ColumnIndex, e.RowIndex)