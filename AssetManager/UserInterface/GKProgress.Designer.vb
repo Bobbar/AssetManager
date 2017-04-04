d8460716754d38920dae09894d14ce2733891141
Bobbar <bobby.lovell@worthingtonindustries.com>
2017-04-04 09:28:25 -0400

    GKUpdater working and refined

--- AssetManager/UserInterface/GKProgress.Designer.vb
+++ AssetManager/UserInterface/GKProgress.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class GKProgress
-    Inherits System.Windows.Forms.Form
+    Inherits MyForm
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,55 +20,55 @@ Partial Class GKProgress
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(GKProgress))
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.cmdGo = New System.Windows.Forms.Button()
         Me.txtPassword = New System.Windows.Forms.TextBox()
         Me.txtUsername = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
         Me.lblCurrentFile = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.pbarProgress = New System.Windows.Forms.ProgressBar()
         Me.lstLog = New System.Windows.Forms.ListBox()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.lblServerDrive = New System.Windows.Forms.Label()
-        Me.lblClientDrive = New System.Windows.Forms.Label()
-        Me.tmrStatus = New System.Windows.Forms.Timer(Me.components)
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.cmdGo)
         Me.GroupBox1.Controls.Add(Me.txtPassword)
         Me.GroupBox1.Controls.Add(Me.txtUsername)
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.Location = New System.Drawing.Point(13, 21)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(511, 172)
+        Me.GroupBox1.Size = New System.Drawing.Size(579, 172)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "LA Credentials"
         '
-        'Button1
+        'cmdGo
         '
-        Me.Button1.Location = New System.Drawing.Point(289, 79)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(145, 35)
-        Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Go"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.cmdGo.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.cmdGo.Location = New System.Drawing.Point(339, 80)
+        Me.cmdGo.Name = "cmdGo"
+        Me.cmdGo.Size = New System.Drawing.Size(145, 35)
+        Me.cmdGo.TabIndex = 4
+        Me.cmdGo.Text = "Go"
+        Me.cmdGo.UseVisualStyleBackColor = True
         '
         'txtPassword
         '
-        Me.txtPassword.Location = New System.Drawing.Point(35, 107)
+        Me.txtPassword.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.txtPassword.Location = New System.Drawing.Point(85, 108)
         Me.txtPassword.Name = "txtPassword"
         Me.txtPassword.Size = New System.Drawing.Size(162, 23)
         Me.txtPassword.TabIndex = 3
@@ -76,15 +76,17 @@ Partial Class GKProgress
         '
         'txtUsername
         '
-        Me.txtUsername.Location = New System.Drawing.Point(35, 54)
+        Me.txtUsername.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.txtUsername.Location = New System.Drawing.Point(85, 55)
         Me.txtUsername.Name = "txtUsername"
         Me.txtUsername.Size = New System.Drawing.Size(162, 23)
         Me.txtUsername.TabIndex = 2
         '
         'Label2
         '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.Top
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(32, 89)
+        Me.Label2.Location = New System.Drawing.Point(82, 90)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(70, 15)
         Me.Label2.TabIndex = 1
@@ -92,8 +94,9 @@ Partial Class GKProgress
         '
         'Label1
         '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.Top
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(32, 36)
+        Me.Label1.Location = New System.Drawing.Point(82, 37)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(70, 15)
         Me.Label1.TabIndex = 0
@@ -101,8 +104,9 @@ Partial Class GKProgress
         '
         'GroupBox2
         '
-        Me.GroupBox2.Controls.Add(Me.lblClientDrive)
-        Me.GroupBox2.Controls.Add(Me.lblServerDrive)
+        Me.GroupBox2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.GroupBox2.Controls.Add(Me.Label4)
         Me.GroupBox2.Controls.Add(Me.lblCurrentFile)
         Me.GroupBox2.Controls.Add(Me.Label3)
@@ -110,24 +114,35 @@ Partial Class GKProgress
         Me.GroupBox2.Controls.Add(Me.lstLog)
         Me.GroupBox2.Location = New System.Drawing.Point(13, 199)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(511, 397)
+        Me.GroupBox2.Size = New System.Drawing.Size(579, 430)
         Me.GroupBox2.TabIndex = 1
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Status"
         '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(18, 112)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(28, 15)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Log"
+        '
         'lblCurrentFile
         '
-        Me.lblCurrentFile.Location = New System.Drawing.Point(234, 73)
+        Me.lblCurrentFile.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblCurrentFile.Location = New System.Drawing.Point(18, 73)
         Me.lblCurrentFile.Name = "lblCurrentFile"
-        Me.lblCurrentFile.Size = New System.Drawing.Size(251, 21)
+        Me.lblCurrentFile.Size = New System.Drawing.Size(545, 21)
         Me.lblCurrentFile.TabIndex = 3
-        Me.lblCurrentFile.Text = "[current file]"
         Me.lblCurrentFile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Label3
         '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.Top
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(234, 24)
+        Me.Label3.Location = New System.Drawing.Point(256, 24)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(63, 15)
         Me.Label3.TabIndex = 2
@@ -135,62 +150,39 @@ Partial Class GKProgress
         '
         'pbarProgress
         '
-        Me.pbarProgress.Location = New System.Drawing.Point(234, 41)
+        Me.pbarProgress.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pbarProgress.Location = New System.Drawing.Point(18, 42)
         Me.pbarProgress.Name = "pbarProgress"
-        Me.pbarProgress.Size = New System.Drawing.Size(251, 30)
+        Me.pbarProgress.Size = New System.Drawing.Size(545, 30)
         Me.pbarProgress.TabIndex = 1
         '
         'lstLog
         '
+        Me.lstLog.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.lstLog.FormattingEnabled = True
         Me.lstLog.ItemHeight = 15
         Me.lstLog.Location = New System.Drawing.Point(18, 130)
         Me.lstLog.Name = "lstLog"
-        Me.lstLog.Size = New System.Drawing.Size(467, 244)
+        Me.lstLog.Size = New System.Drawing.Size(545, 274)
         Me.lstLog.TabIndex = 0
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(234, 108)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(28, 15)
-        Me.Label4.TabIndex = 4
-        Me.Label4.Text = "Log"
-        '
-        'lblServerDrive
-        '
-        Me.lblServerDrive.AutoSize = True
-        Me.lblServerDrive.Location = New System.Drawing.Point(15, 41)
-        Me.lblServerDrive.Name = "lblServerDrive"
-        Me.lblServerDrive.Size = New System.Drawing.Size(182, 15)
-        Me.lblServerDrive.TabIndex = 5
-        Me.lblServerDrive.Text = "Server Map:  Disconnected"
-        '
-        'lblClientDrive
-        '
-        Me.lblClientDrive.AutoSize = True
-        Me.lblClientDrive.Location = New System.Drawing.Point(15, 74)
-        Me.lblClientDrive.Name = "lblClientDrive"
-        Me.lblClientDrive.Size = New System.Drawing.Size(182, 15)
-        Me.lblClientDrive.TabIndex = 6
-        Me.lblClientDrive.Text = "Client Map:  Disconnected"
-        '
-        'tmrStatus
-        '
-        Me.tmrStatus.Enabled = True
-        Me.tmrStatus.Interval = 200
         '
         'GKProgress
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(547, 631)
+        Me.ClientSize = New System.Drawing.Size(606, 647)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
+        Me.DoubleBuffered = True
         Me.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MinimumSize = New System.Drawing.Size(622, 686)
         Me.Name = "GKProgress"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Gatekeeper Updater"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
@@ -205,14 +197,11 @@ Partial Class GKProgress
     Friend WithEvents txtUsername As TextBox
     Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
-    Friend WithEvents Button1 As Button
+    Friend WithEvents cmdGo As Button
     Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents lblCurrentFile As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents pbarProgress As ProgressBar
     Friend WithEvents lstLog As ListBox
     Friend WithEvents Label4 As Label
-    Friend WithEvents lblServerDrive As Label
-    Friend WithEvents lblClientDrive As Label
-    Friend WithEvents tmrStatus As Timer
 End Class
--- AssetManager/UserInterface/GKProgress.vb
+++ AssetManager/UserInterface/GKProgress.vb
@@ -1,69 +1,277 @@
-﻿Public Class GKProgress
-    Public MaxFiles As Integer = 1
-    Public CurrentFileIdx As Integer = 0
-    Public CurrentFile As String = ""
-    Public PrevFile As String = ""
-    Private Client As String
-    Sub New(ClientName As String)
+﻿Imports System.Net
+Imports System.IO
+Imports System.ComponentModel
+Public Class GKProgress
+    Private WithEvents CopyWorker As BackgroundWorker
+    Private ReadOnly GKPath As String = "\PSi\Gatekeeper"
+    Private ReadOnly ServerName As String = "10.10.0.184"
+    '"ddad-svr-fs01"
+    Private ReadOnly ShareName As String = "\c$"
 
+    Private AdmPassword As String
+    Private AdmUsername As String
+    Private ClientPath As String
+    Private CurrentFileIdx As Integer = 0
+    Private CurrentStatus As Status_Stats
+    Private MaxFiles As Integer = 1
+    Private MyDevice As Device_Info
+    Private PrevFile As String = ""
+    Private ServerPath As String
+    Sub New(ParentForm As MyForm, Device As Device_Info)
         ' This call is required by the designer.
         InitializeComponent()
-
         ' Add any initialization after the InitializeComponent() call.
-
-        Client = ClientName
+        Tag = ParentForm
+        Icon = ParentForm.Icon
+        MyDevice = Device
+        ServerPath = "\\" & ServerName & "\c$"
+        ClientPath = "\\D" & MyDevice.strSerial & "\c$"
+        Text = Text + " - " + MyDevice.strCurrentUser
+        InitWorker()
         Show()
-
-
-    End Sub
-    Private Sub RunUpdate()
-        lstLog.Items.Add("Mapping drives...")
-        txtUsername.ReadOnly = True
-        txtPassword.ReadOnly = True
-        Dim GKUpdate As New GK_Updater(Trim(txtUsername.Text), Trim(txtPassword.Text), Client)
-        lblServerDrive.Text = "Server Map: Connecting..."
-        If GKUpdate.ConnectServerDrive Then lblServerDrive.Text = "Server Map: Connected!"
-        lblClientDrive.Text = "Client Map: Connecting..."
-        If GKUpdate.ConnectClientDrive Then lblClientDrive.Text = "Client Map: Connected!"
-        lstLog.Items.Add("Copying files...")
-        If GKUpdate.CopyFiles(CurrentFile, MaxFiles, CurrentFileIdx) Then
-            lstLog.Items.Add("Copy successful!")
-            lstLog.Items.Add("Disconnecting Maps...")
-            lblServerDrive.Text = "Server Map: Disconnecting..."
-            If GKUpdate.RemoveServerDrive Then
-                lblServerDrive.Text = "Server Map: Disconnected"
-            Else
-                lstLog.Items.Add("Server Map Disconnect Failed!")
-            End If
-
-            lblClientDrive.Text = "Client Map: Disconnecting..."
-            If GKUpdate.RemoveClientDrive Then
-                lblClientDrive.Text = "Client Map: Disconnected"
-            Else
-                lstLog.Items.Add("Client Map Disconnect Failed!")
-            End If
-            lstLog.Items.Add("All done!")
-
-        Else
-
-            lstLog.Items.Add("Copy Failed!")
-        End If
-
-
     End Sub
 
-    Private Sub tmrStatus_Tick(sender As Object, e As EventArgs) Handles tmrStatus.Tick
-        pbarProgress.Maximum = MaxFiles
-        pbarProgress.Value = CurrentFileIdx
+    Public Sub CopyFiles()
+        Try
+            If Not CopyWorker.IsBusy Then CopyWorker.RunWorkerAsync()
+        Catch ex As Exception
+            ErrHandle(ex, System.Reflection.MethodInfo.GetCurrentMethod().Name)
+        End Try
+    End Sub
+
+    Private Sub cmdGo_Click(sender As Object, e As EventArgs) Handles cmdGo.Click
+        RunUpdate()
+    End Sub
+
+    Private Function ConnectClientDrive() As Boolean
+        Try
+            TopMost = True
+            Dim pClient As Process = New Process
+            pClient.StartInfo.UseShellExecute = False
+            pClient.StartInfo.RedirectStandardOutput = True
+            pClient.StartInfo.RedirectStandardError = True
+            pClient.StartInfo.WindowStyle = ProcessWindowStyle.Minimized
+            pClient.StartInfo.FileName = "net.exe"
+            pClient.StartInfo.Arguments = "USE " & ClientPath & " " & AdmPassword & " /USER:" & AdmUsername
+            pClient.Start()
+            Dim output As String
+            output = pClient.StandardError.ReadToEnd
+            pClient.WaitForExit()
+            TopMost = False
+            Debug.Print(output)
+            If Trim(output) = "" Then
+                Return True
+            Else
+                GKLog(output)
+                Return False
+            End If
+            Return False
+        Catch ex As Exception
+            ErrHandle(ex, System.Reflection.MethodInfo.GetCurrentMethod().Name)
+            Return False
+        End Try
+    End Function
+
+    Private Function ConnectServerDrive() As Boolean
+        Try
+            TopMost = True
+            Dim pServer As Process = New Process
+            pServer.StartInfo.UseShellExecute = False
+            pServer.StartInfo.RedirectStandardOutput = True
+            pServer.StartInfo.RedirectStandardError = True
+            pServer.StartInfo.WindowStyle = ProcessWindowStyle.Minimized
+            pServer.StartInfo.FileName = "net.exe"
+            pServer.StartInfo.Arguments = "USE " & ServerPath & " " & AdmPassword & " /USER:" & AdmUsername
+            pServer.Start()
+            Dim output As String
+            output = pServer.StandardError.ReadToEnd
+            pServer.WaitForExit()
+            TopMost = False
+            Debug.Print(output)
+            If Trim(output) = "" Then
+                Return True
+            Else
+                GKLog(output)
+                Return False
+            End If
+            Return False
+        Catch ex As Exception
+            ErrHandle(ex, System.Reflection.MethodInfo.GetCurrentMethod().Name)
+            Return False
+        End Try
+    End Function
+
+    Private Sub CopyWorker_DoWork(sender As Object, e As DoWorkEventArgs)
+        Dim CurFileIdx As Integer = 0
+        Dim sourceDir As String = ServerPath & GKPath
+        Dim targetDir As String = ClientPath & GKPath
+        Dim files As String() = Directory.GetFiles(sourceDir)
+        For Each file__1 In files
+            CurFileIdx += 1
+            Dim Status As New Status_Stats(files.Count, CurFileIdx, file__1)
+            CopyWorker.ReportProgress(1, Status)
+            'Debug.Print("Copying: " & file__1)
+            File.Copy(file__1, Path.Combine(targetDir, Path.GetFileName(file__1)), True)
+        Next
+    End Sub
+
+    Private Sub CopyWorker_ProgressChanged(sender As Object, e As ProgressChangedEventArgs)
+        CurrentStatus = DirectCast(e.UserState, Status_Stats)
+        pbarProgress.Maximum = CurrentStatus.TotFiles
+        pbarProgress.Value = CurrentStatus.CurFileIdx
+        Dim CurrentFile = CurrentStatus.CurFileName
         lblCurrentFile.Text = CurrentFile
         If PrevFile <> CurrentFile Then
-            lstLog.Items.Add(CurrentFile)
+            GKLog(CurrentStatus.CurFileIdx & " of " & CurrentStatus.TotFiles & ": " & CurrentFile)
             PrevFile = CurrentFile
         End If
         Application.DoEvents()
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        RunUpdate()
+    Private Sub CopyWorker_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs)
+        If e.Error Is Nothing Then
+            GKLog("Copy successful!")
+            GKLog("Disconnecting Maps...")
+            GKLog("Server Map: Disconnecting...")
+            If RemoveServerDrive() Then
+                GKLog("Server Map: Disconnected")
+            Else
+                GKLog("Server Map Disconnect Failed!")
+            End If
+            GKLog("Client Map: Disconnecting...")
+            If RemoveClientDrive() Then
+                GKLog("Client Map: Disconnected")
+            Else
+                GKLog("Client Map Disconnect Failed!")
+            End If
+            GKLog("All done!")
+            GKLog("------------------------------------------------")
+            Text = Text + " - *COMPLETE*"
+        Else
+            Text = Text + " - *ERRORS!*"
+            GKLog("------------------------------------------------")
+            GKLog("Errors during copy!")
+            GKLog(e.Error.Message)
+        End If
     End Sub
+
+    Private Sub GKLog(Message As String)
+        lstLog.Items.Add(Message)
+        lstLog.TopIndex = lstLog.Items.Count - 1
+        Logger(Message)
+        Application.DoEvents()
+    End Sub
+
+    Private Sub InitWorker()
+        CopyWorker = New BackgroundWorker
+        AddHandler CopyWorker.DoWork, AddressOf CopyWorker_DoWork
+        AddHandler CopyWorker.RunWorkerCompleted, AddressOf CopyWorker_RunWorkerCompleted
+        AddHandler CopyWorker.ProgressChanged, AddressOf CopyWorker_ProgressChanged
+        With CopyWorker
+            .WorkerReportsProgress = True
+            .WorkerSupportsCancellation = True
+        End With
+    End Sub
+
+    Private Function RemoveClientDrive() As Boolean
+        Try
+            TopMost = True
+            Dim pClient As Process = New Process
+            pClient.StartInfo.UseShellExecute = False
+            pClient.StartInfo.RedirectStandardOutput = True
+            pClient.StartInfo.RedirectStandardError = True
+            pClient.StartInfo.WindowStyle = ProcessWindowStyle.Minimized
+            pClient.StartInfo.FileName = "net.exe"
+            pClient.StartInfo.Arguments = "USE " & ClientPath & " /delete"
+            pClient.Start()
+            Dim output As String
+            output = pClient.StandardError.ReadToEnd
+            pClient.WaitForExit()
+            TopMost = False
+            Debug.Print(output)
+            If Trim(output) = "" Then
+                Return True
+            Else
+                GKLog(output)
+                Return False
+            End If
+            Return False
+        Catch ex As Exception
+            ErrHandle(ex, System.Reflection.MethodInfo.GetCurrentMethod().Name)
+            Return False
+        End Try
+    End Function
+
+    Private Function RemoveServerDrive() As Boolean
+        Try
+            TopMost = True
+            Dim pServer As Process = New Process
+            pServer.StartInfo.UseShellExecute = False
+            pServer.StartInfo.RedirectStandardOutput = True
+            pServer.StartInfo.RedirectStandardError = True
+            pServer.StartInfo.WindowStyle = ProcessWindowStyle.Minimized
+            pServer.StartInfo.FileName = "net.exe"
+            pServer.StartInfo.Arguments = "USE " & ServerPath & " /delete"
+            pServer.Start()
+            Dim output As String
+            output = pServer.StandardError.ReadToEnd
+            pServer.WaitForExit()
+            TopMost = False
+            Debug.Print(output)
+            If Trim(output) = "" Then
+                Return True
+            Else
+                GKLog(output)
+                Return False
+            End If
+            Return False
+        Catch ex As Exception
+            ErrHandle(ex, System.Reflection.MethodInfo.GetCurrentMethod().Name)
+            Return False
+        End Try
+    End Function
+
+    Private Sub RunUpdate()
+        Try
+            GKLog("------------------------------------------------")
+            GKLog("Starting GK Update to: " & MyDevice.strSerial)
+            GKLog("Mapping drives...")
+            txtUsername.ReadOnly = True
+            txtPassword.ReadOnly = True
+            cmdGo.Enabled = False
+            AdmUsername = Trim(txtUsername.Text)
+            AdmPassword = Trim(txtPassword.Text)
+            GKLog("Server Map: Connecting...")
+            If ConnectServerDrive() Then
+                GKLog("Server Map: Connected!")
+            Else
+                GKLog("Server Map Failed!")
+                Exit Sub
+            End If
+            GKLog("Client Map: Connecting...")
+            If ConnectClientDrive() Then
+                GKLog("Client Map: Connected!")
+            Else
+                GKLog("Client Map Failed!")
+                Exit Sub
+            End If
+            GKLog("Copying files...")
+
+            CopyFiles()
+            BringToFront()
+        Catch ex As Exception
+            ErrHandle(ex, System.Reflection.MethodInfo.GetCurrentMethod().Name)
+        End Try
+    End Sub
+
+    Private Structure Status_Stats
+        Public CurFileIdx As Integer
+        Public CurFileName As String
+        Public TotFiles As Integer
+        Sub New(tFiles As Integer, CurFIdx As Integer, CurFName As String)
+            TotFiles = tFiles
+            CurFileIdx = CurFIdx
+            CurFileName = CurFName
+        End Sub
+    End Structure
 End Class
