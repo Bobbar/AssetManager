56fc519fd346e17a5b0cc1bf037778dae1c388ea
Bobbar <bobby.lovell@worthingtonindustries.com>
2017-04-07 08:39:25 -0400

    First Commit

---
+++ AssetManager/Classes/Test_Class.vb
@@ -0,0 +1,17 @@
+﻿Public Class Test_Class
+    Sub New(ByRef ValueToUpdate As Integer)
+        ReturnNumbers(ValueToUpdate)
+
+    End Sub
+
+
+    Private Function ReturnNumbers(ByRef Value As Integer)
+        For i As Integer = 0 To 20
+
+            Return i
+
+        Next
+
+
+    End Function
+End Class
