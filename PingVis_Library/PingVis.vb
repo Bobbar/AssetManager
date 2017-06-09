111249c9e6dae423583923da41eb58a9b7e4ff38
Bobbar <bobbar@gmail.com>
2017-06-09 15:03:17 -0400

    Revert "Clear ScrollingBars list in PingVis on Dispose."
    This reverts commit 2863030098f9e33b8569b0d07a82b06813841ddc.

--- PingVis_Library/PingVis.vb
+++ PingVis_Library/PingVis.vb
@@ -432,7 +432,6 @@ Public Class PingVis : Implements IDisposable
                 MyPing.Dispose()
                 ' MyControl.Dispose()
                 pngResults.Clear()
-                ScrollingBars.Clear()
             End If
 
             ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
