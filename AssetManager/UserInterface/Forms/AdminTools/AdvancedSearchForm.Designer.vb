186a6a4224ca2b1c46634b725b41af3f2c89e92e
Bobbar <bobbar@gmail.com>
2017-07-13 11:22:28 -0400

    Changed all forms to MyForm.

--- AssetManager/UserInterface/Forms/AdminTools/AdvancedSearchForm.Designer.vb
+++ AssetManager/UserInterface/Forms/AdminTools/AdvancedSearchForm.Designer.vb
@@ -1,6 +1,6 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class AdvancedSearchForm
-    Inherits System.Windows.Forms.Form
+    Inherits MyForm
 
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()>
--- AssetManager/UserInterface/Forms/AssetManager/TrackDeviceForm.Designer.vb
+++ AssetManager/UserInterface/Forms/AssetManager/TrackDeviceForm.Designer.vb
@@ -1,6 +1,6 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class TrackDeviceForm
-    Inherits System.Windows.Forms.Form
+    Inherits MyForm
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
