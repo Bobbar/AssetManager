9c8fa628abb3e4f0033adfd8a36787839ed4ac8b
Bobbar <bobbar@gmail.com>
2017-09-14 09:45:22 -0400

    Switch back to prod DB.

--- AssetManager/DataComms/AssetManager/ServerInfo.vb
+++ AssetManager/DataComms/AssetManager/ServerInfo.vb
@@ -3,6 +3,6 @@
         Public ServerPinging As Boolean = False
         Public CurrentDataBase As String
         Public Const MySQLServerIP As String = "10.10.0.89"
-        Public Const UseTestDatabase As Boolean = True
+        Public Const UseTestDatabase As Boolean = False
     End Module
 End Namespace
