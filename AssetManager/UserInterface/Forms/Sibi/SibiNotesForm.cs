5748a5f1a9c4bca9ae900f737185e8aa1abf3830
Bobbar <bobbar@gmail.com>
2017-11-15 16:02:15 -0500

    Fix SibiNotes and SibiSelector events. Done with events finally?!

--- AssetManager/UserInterface/Forms/Sibi/SibiNotesForm.cs
+++ AssetManager/UserInterface/Forms/Sibi/SibiNotesForm.cs
@@ -12,69 +12,73 @@ using AssetManager.UserInterface.CustomControls;
 
 namespace AssetManager.UserInterface.Forms.Sibi
 {
-	public partial class SibiNotesForm : ExtendedForm
-	{
+    public partial class SibiNotesForm : ExtendedForm
+    {
 
-		private RequestObject MyRequest;
-		public RequestObject Request {
-			get { return MyRequest; }
-		}
+        private RequestObject MyRequest;
+        public RequestObject Request
+        {
+            get { return MyRequest; }
+        }
 
-		public SibiNotesForm(ExtendedForm parentForm, RequestObject request)
-		{
-			InitializeComponent();
-			this.ParentForm = parentForm;
-			MyRequest = request;
-			ShowDialog(parentForm);
-		}
+        public SibiNotesForm(ExtendedForm parentForm, RequestObject request)
+        {
+            InitializeComponent();
+            this.ParentForm = parentForm;
+            MyRequest = request;
+            ShowDialog(parentForm);
+        }
 
-		public SibiNotesForm(ExtendedForm parentForm, string noteUID)
-		{
-			InitializeComponent();
-			this.ParentForm = parentForm;
-			FormUID = noteUID;
-			ViewNote(noteUID);
-		}
+        public SibiNotesForm(ExtendedForm parentForm, string noteUID)
+        {
+            InitializeComponent();
+            this.ParentForm = parentForm;
+            FormUID = noteUID;
+            ViewNote(noteUID);
+        }
 
-		private void ClearAll()
-		{
-			rtbNotes.Clear();
-		}
+        private void ClearAll()
+        {
+            rtbNotes.Clear();
+        }
 
-		private void ViewNote(string noteUID)
-		{
-			try {
-				cmdOK.Visible = false;
-				rtbNotes.Clear();
-				string NoteText = GlobalInstances.AssetFunc.GetSqlValue(SibiNotesCols.TableName, SibiNotesCols.NoteUID, noteUID, SibiNotesCols.Note);
-				string NoteTimeStamp = GlobalInstances.AssetFunc.GetSqlValue(SibiNotesCols.TableName, SibiNotesCols.NoteUID, noteUID, SibiNotesCols.DateStamp);
-				this.Text += " - " + NoteTimeStamp;
-				OtherFunctions.SetRichTextBox(rtbNotes, NoteText);
-				rtbNotes.ReadOnly = true;
-				rtbNotes.BackColor = Color.White;
-				Show();
-				Activate();
-			} catch (Exception ex) {
-				ErrorHandling.ErrHandle(ex, System.Reflection.MethodInfo.GetCurrentMethod());
-			}
-		}
+        private void ViewNote(string noteUID)
+        {
+            try
+            {
+                cmdOK.Visible = false;
+                rtbNotes.Clear();
+                string NoteText = GlobalInstances.AssetFunc.GetSqlValue(SibiNotesCols.TableName, SibiNotesCols.NoteUID, noteUID, SibiNotesCols.Note);
+                string NoteTimeStamp = GlobalInstances.AssetFunc.GetSqlValue(SibiNotesCols.TableName, SibiNotesCols.NoteUID, noteUID, SibiNotesCols.DateStamp);
+                this.Text += " - " + NoteTimeStamp;
+                OtherFunctions.SetRichTextBox(rtbNotes, NoteText);
+                rtbNotes.ReadOnly = true;
+                rtbNotes.BackColor = Color.White;
+                Show();
+                Activate();
+            }
+            catch (Exception ex)
+            {
+                ErrorHandling.ErrHandle(ex, System.Reflection.MethodInfo.GetCurrentMethod());
+            }
+        }
 
-		private void cmdOK_Click(object sender, EventArgs e)
-		{
-			DialogResult = DialogResult.OK;
-			this.Close();
-		}
+        private void cmdOK_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            this.Close();
+        }
 
-		private void cmdClose_Click(object sender, EventArgs e)
-		{
-			DialogResult = DialogResult.Abort;
-			this.Dispose();
-		}
+        private void cmdClose_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Abort;
+            this.Dispose();
+        }
 
-		private void rtbNotes_LinkClicked(object sender, LinkClickedEventArgs e)
-		{
-			Process.Start(e.LinkText);
-		}
+        private void rtbNotes_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            Process.Start(e.LinkText);
+        }
 
-	}
+    }
 }
